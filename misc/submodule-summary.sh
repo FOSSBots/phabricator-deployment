ff7d4cb8025d9683c7921f663ca1c1506a686c44
Mukunda Modell <mmodell@wikimedia.org>
2016-04-07 23:53:30 -0500

    submodule updates

--- misc/submodule-summary.sh
+++ misc/submodule-summary.sh
@@ -1,1 +1,1 @@
-git submodule foreach -q 'tag=`git describe`; git log --pretty="| $path | %h | $tag | %<(40,trunc)%s |" -1'
+git submodule foreach -q 'tag=`git describe`; GIT_PAGER="less -E" git --no-pager log --pretty="| $path | %h | $tag | %<(40,trunc)%s |" -1'
