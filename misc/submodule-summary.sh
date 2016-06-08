96cd34bdc541b6f4f9bb5eee6546bda90a31eb5f
Mukunda Modell <mmodell@wikimedia.org>
2016-06-08 18:54:42 -0500

    Fix submodules for release/2016-06-08/1

--- misc/submodule-summary.sh
+++ misc/submodule-summary.sh
@@ -1,1 +1,2 @@
-git submodule foreach -q 'tag=`git describe`; GIT_PAGER="less -E" git --no-pager log --pretty="| $path | %h | $tag | %<(40,trunc)%s |" -1'
+echo '|**module**|**tag**|**commit**|'
+git submodule foreach -q 'tag=`git describe`; GIT_PAGER="less -E" git --no-pager log --pretty="| $path | $tag | {%h} |" -1'
