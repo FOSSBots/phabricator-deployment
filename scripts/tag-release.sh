bbe455847535adefdbc428cab320c075ffb0b2f5
Mukunda Modell <mmodell@wikimedia.org>
2016-11-15 09:35:15 -0600

    updated for deployment on 11-16-2016

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 TAGDATE=`date +%Y-%m-%d`
 REV=1
-TAG=release/$TAGDATE/$REV
+TAGDATE=${1-$TAGDATE}
+TAG="release/$TAGDATE/$REV"
 git submodule foreach "git tag -d $TAG" > /dev/null 2>&1
 TAGCMD="git tag -a $TAG -m \"tagging $TAG\""
 git submodule foreach "$TAGCMD"
