265d0fb18dd880d986bccceac6998d3378284926
Mukunda Modell <mmodell@wikimedia.org>
2017-02-08 18:01:36 -0600

    Prepare #phabricator milestone #phab-20170208
    Submodule revisions will be tagged `release/2017-02-08/1`

--- scap/plugins/tag.py
+++ scap/plugins/tag.py
@@ -3,16 +3,25 @@
 from __future__ import print_function, unicode_literals
 
 import datetime
-import logging
-import sys
-from scap import ansi, cli, log
+import os
+
+from string import Template
+
+from phabricator import Phabricator
+from scap import cli, log
 from scap.utils import cd
 from sh import git
-from .asciitable import AsciiTable
+from scapext.utils import dump_json
+from scapext.asciitable import AsciiTable
+
+import sh
+
+submodule_summary = sh.Command('scripts/submodule-summary.sh')
 
 submod = git.bake('submodule', 'foreach', '-q')
 
 submodule_paths = []
+
 
 def all_submodules():
     global submodule_paths
@@ -21,18 +30,21 @@ def all_submodules():
     for mod in submodule_paths:
         yield mod
 
+
 def repo_plus_submodules():
     yield './'
     for mod in all_submodules():
         yield mod
 
+
 def trunc(len, string, ellipsis=" …"):
     return string[0:len] + ellipsis
+
 
 def action_arg(*args, **kwargs):
     kwargs['const'] = kwargs['action']
     kwargs['dest'] = 'action'
-    kwargs['action'] ='store_const'
+    kwargs['action'] = 'store_const'
     kwargs['metavar'] = 'ACTION'
     return cli.argument(*args, **kwargs)
 
@@ -49,12 +61,11 @@ class ReleaseTagger(cli.Application):
         """Setup logging."""
         log.setup_loggers(self.config, self.arguments.loglevel + 10)
 
-
     def _process_arguments(self, args, extra_args):
-        #print(args,extra_args)
-        return args,extra_args
+        # print(args,extra_args)
+        return args, extra_args
 
-    #@cli.argument('-l', help='list tags', action='store_true')
+    # @cli.argument('-l', help='list tags', action='store_true')
 
     @cli.subcommand()
     def list(self, *args):
@@ -71,11 +82,13 @@ class ReleaseTagger(cli.Application):
 
     @cli.argument('msg', metavar='TEXT', nargs='?', default='',
                   help='Tag Annotation / Commit message.')
-    @cli.argument('--date', nargs=1, default=None, type=str, metavar='YYYY-MM-DD',
-                help="Specify the date for your tag, default: today's date")
-    @cli.argument('-s', nargs=1, type=int, default=1, dest='seq', metavar='SEQ',
-                help='Sequence number: Increase this number when you tag\
-                more than one release on the same date.')
+    @cli.argument('--date', nargs=1, default=None, type=str,
+                  metavar='YYYY-MM-DD',
+                  help="Specify the date for your tag, default: today's date")
+    @cli.argument('-s', nargs=1, type=int, default=1, dest='seq',
+                  metavar='SEQ',
+                  help='Sequence number: Increase this number when you tag\
+                  more than one release on the same date.')
     @cli.argument('-f', '--force', action='store_true',
                   help='Continue operation ignoring any errors.')
     def create(self, *args):
@@ -101,19 +114,86 @@ class ReleaseTagger(cli.Application):
                     else:
                         raise ex
 
-    @cli.argument('--date', nargs=1, default=None, type=str, metavar='YYYY-MM-DD',
-                help="Specify the date for your tag, default: today's date")
-    @cli.argument('-s', nargs=1, type=int, default=1, dest='seq', metavar='SEQ',
-                help='Sequence number: Increase this number when you tag\
-                more than one release on the same date.')
+    @cli.argument('--date', nargs=1, default=None, type=str,
+                  metavar='YYYY-MM-DD',
+                  help="Specify the date for your tag, default: today's date")
+    @cli.argument('-s', nargs=1, type=int, default=1, dest='seq',
+                  metavar='SEQ',
+                  help='Sequence number: Increase this number when you tag\
+                  more than one release on the same date.')
     @cli.argument('-f', '--force', action='store_true',
                   help='Continue operation ignoring any errors.')
     def delete(self, *args):
         ''' delete tag in this repo plus all submodules '''
-        tag,date,seq = self.get_calver_tag()
+        tag, date, seq = self.get_calver_tag()
         for path in repo_plus_submodules():
             with cd(path):
-                print(git('tag','-d', tag))
+                print(git('tag', '-d', tag))
+
+    @cli.argument('--project', nargs=1, default='phabricator', dest='project',
+                  metavar='PROJECT', help='Create a milestone within PROJECT')
+    @cli.argument('--template', nargs=1, default='phab.tmpl', dest='template',
+                  metavar='template', help='template file', type=file)
+    @cli.argument('milestone', nargs=1, type=str,
+                  help='The name of a milestone to create')
+    @cli.argument('--hashtag', nargs=1, default=None,
+                  help='Hashtag used to refer to the milestone. Defaults to \
+                  ProjectName-MilestoneName')
+    def milestone(self, *args):
+        ''' create a milestone in phabricator '''
+        phab = Phabricator()  # This will use your ~/.arcrc file
+        project_name = self.arguments.project
+        query = {
+            'name': project_name,
+            'isMilestone': False
+        }
+        project = phab.project.search(constraints=query)
+
+        template = Template("".join(self.arguments.template[0].readlines()))
+        # dump_json(vars(self.arguments).keys())
+        tmpl_vars = {}
+        for key in vars(self.arguments).keys():
+            val = getattr(self.arguments, key)
+            while(type(val) is list):
+                val = val[0]
+
+            if type(val) in (unicode, str):
+                tmpl_vars[key] = val
+
+        tmpl_vars['submodule_summary'] = submodule_summary()
+
+        dump_json(tmpl_vars)
+        milestone_name = self.arguments.milestone[0]
+        description = template.safe_substitute(tmpl_vars)
+
+        print(description)
+
+        if self.arguments.hashtag is None:
+            hashtag = project_name + '-' + milestone_name
+        else:
+            hashtag = self.arguments.hashtag
+
+        result = phab.project.edit(transactions=[
+            {
+                 'type': 'milestone',
+                 'value': project['data'][0]['phid']
+            },
+            {
+                'type': 'name',
+                'value': milestone_name
+            },
+            {
+                'type': 'slugs',
+                'value': [hashtag]
+            },
+            {
+                'type': 'description',
+                'value': description
+            }
+
+        ])
+
+        dump_json(result)
 
     def get_calver_tag(self):
         ''' generate a tag string with the format release/{date}/{sequence} '''
@@ -121,17 +201,18 @@ class ReleaseTagger(cli.Application):
         seq = self.arguments.seq
 
         if (date is None):
-            date=datetime.datetime.today()
-            date_format="%Y-%m-%d"
+            date = datetime.datetime.today()
+            date_format = "%Y-%m-%d"
             date = date.strftime(date_format)
         else:
-            date=date[0]
+            date = date[0]
         if len(date) != 10:
-            raise ValueError('Date must be 10 digits, YYYY-MM-DD. You entered: %s' % date)
+            msg = 'Date must be 10 digits, YYYY-MM-DD. You entered: %s' % date
+            raise ValueError(msg)
 
         if (type(seq) is not int):
             raise ValueError('Sequence must be numeric value > 0')
 
-        tag_format="release/{date}/{seq}"
+        tag_format = "release/{date}/{seq}"
 
         return (tag_format.format(date=date, seq=seq), date, seq)
