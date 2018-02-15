6a0d37beac08d6dc77914a6a52467d1335a2a4d4
Mukunda Modell <mmodell@wikimedia.org>
2018-02-14 20:57:53 -0600

    updated tag-release script and disabled a migration in phabricator/

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-TAGDATE=`date +%Y-%m-%d`
+TAGDATE=`date +%Y-%m-%d -u`
 DEFAULTREV=1
 REV=${2-$DEFAULTREV}
 TAGDATE=${1-$TAGDATE}
 TAG="release/$TAGDATE/$REV"
 git submodule foreach "git tag -d $TAG" > /dev/null 2>&1
-TAGCMD="git tag -a $TAG -m \"tagging $TAG\""
+TAGCMD="git tag -a $TAG --sign -m \"tagging $TAG\""
 git submodule foreach "$TAGCMD"
 git tag -d $TAG >/dev/null 2>&1
 git tag -a $TAG -m "tagging for release: $TAG"
