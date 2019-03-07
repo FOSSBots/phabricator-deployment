1a42721bf037027ac63f6e5b3fb0b1b16fd1b447
Mukunda Modell <mmodell@wikimedia.org>
2019-03-06 19:08:53 -0600

    more release script fixes

--- scripts/submodule-summary.sh
+++ scripts/submodule-summary.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo '|**module**|**tag**|**commit**|'
-git submodule foreach -q 'tag=`git describe`; GIT_PAGER="less -E" git --no-pager log --pretty="| $path | $tag | {%h} |" -1'
+git submodule foreach -q 'tag=`git describe`; GIT_PAGER="less -E" git --no-pager log --pretty="| $path | $tag | {%H} |" -1'
--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -3,19 +3,19 @@ git diff --quiet ||
 
 TAGDATE=`date +%Y-%m-%d -u`
 DEFAULTREV=1
-REV=${2-$DEFAULTREV}
-TAGDATE=${1-$TAGDATE}
+REV="${2-$DEFAULTREV}"
+TAGDATE="${1-$TAGDATE}"
 TAG="release/$TAGDATE/$REV"
 git submodule foreach "git tag -d $TAG" > /dev/null 2>&1
 MSG="phabricator.wikimedia.org version $TAG"
 TAGCMD="git tag -a $TAG --sign -m \"$MSG\""
 git submodule foreach "$TAGCMD"
-git tag -d $TAG >/dev/null 2>&1
-git tag -a $TAG --sign -m "$MSG"
+git tag -d "$TAG" >/dev/null 2>&1
+git tag -a "$TAG" --sign -m "$MSG"
 git submodule foreach --quiet 'git describe'
 git describe
 
-git push origin wmf/stable $TAG
-git submodule foreach "git push origin wmf/stable +$TAG"
+git push origin wmf/stable "$TAG"
+git submodule foreach "git push origin wmf/stable $TAG"
 
 scap tag milestone --project phabricator --template ./templates/phab.tmpl $TAGDATE
