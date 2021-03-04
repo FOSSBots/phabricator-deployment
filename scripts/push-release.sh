a200c78bc015c10f0f9edb764a75ab79999becf6
Mukunda Modell <mmodell@wikimedia.org>
2021-03-03 19:00:18 -0600

    release/20210303-1

--- scripts/push-release.sh
+++ scripts/push-release.sh
@@ -1,3 +1,4 @@
-
-git submodule foreach 'git push origin $(git describe) wmf/stable || true'
-git push origin $(git describe) wmf/stable
+#!/bin/bash
+git submodule foreach 'git push origin $(git describe --abbrev=0 --match=release*) wmf/stable || true'
+tag=$(git describe --abbrev=0 --match=release*)
+git push origin $tag wmf/stable
